431acb85884b246631faea40021ea6e60c29e27f
Aleksander Shumeiko <aleksandr.shumeiko@simbirsoft.com>
2024-05-21 09:03:18 +0300

    удалил лишнюю строку в методе getRandomSeconds

--- vue/common/functions.js
+++ vue/common/functions.js
@@ -1,7 +1,6 @@
 export default {
     getRandomSeconds: function(min, max) {
-        return 1;
-        // return (Math.floor(Math.random() * (max - min + 1)) + min) * 1000;
+        return (Math.floor(Math.random() * (max - min + 1)) + min) * 1000;
     },
     getUrl() {
         return `http://${document.location.host}`;
